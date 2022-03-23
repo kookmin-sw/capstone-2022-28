864cec483fc801682d6b95bf1cf8d4af82541a98
bamin0422 <bamin0422@gmail.com>
2022-03-23 19:02:50 +0900

    Edit webpack.config.js for webpack v5

--- exhibit/src/App.js
+++ exhibit/src/App.js
@@ -5,6 +5,24 @@ import ArtPage from "./routes/ArtPage";
 import MyAssetPage from "./routes/MyAssetPage";
 import MyAccount from "./routes/MyAccount";
 import ContestPage from "./routes/ContestPage";
+import Caver from 'caver-js';
+
+const COUNT_CONTRACT_ADDRESS = '0x3aEDa4A4e7EA89fC72C0CAf6e95C11f19493f8d7';
+const ACCESS_KEY_ID = 'KASKO3SVMXYIKU61S1AN326W';
+const SECRET_ACCESS_KEY = 'f5wGsFBfteeRc0bXxKwoK1DG-45JKzqmYNiUEwvs';
+const Authorization = 'Basic S0FTS08zU1ZNWFlJS1U2MVMxQU4zMjZXOmY1d0dzRkJmdGVlUmMwYlh4S3dvSzFERy00NUpLenFtWU5pVUV3dnM='
+
+const CHAIN_ID = '1001' // MAINNET 8217 TESTNNET 1001
+
+/*const option = {
+  headers:[ 
+    {
+      name: "Authorizati~on",
+      value: "Basic "+ Buffer.from(ACCESS_KEY_ID + SECRET_ACCESS_KEY).toString("base64")
+    },
+    { name: "x-chain-id", value: CHAIN_ID }
+  ]
+}*/
 
 function App() {
   return (
