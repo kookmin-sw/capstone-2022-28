5db696b8e73a2830037b617616d74514f5d26d09
roseonghwan <nsh1998@kookmin.ac.kr>
2022-05-19 01:09:26 +0900

    make upload modal

--- exhibit/src/routes/UploadPage/UploadPage.js
+++ exhibit/src/routes/UploadPage/UploadPage.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import Dropzone from "react-dropzone";
 import { Typography, Button, Form, Input } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
 import styles from "./UploadPage.module.css";
 import Auth from "../../hoc/auth";
 import LoginNavigationBar from "../../components/Navbar/LoginNavigationBar";
-import { MultipleFilesUpload, ImageUpload} from "react-ipfs-uploader";
+import { FileUpload, ImageUpload } from "react-ipfs-uploader";
 import axios from "axios";
+import { Modal } from "react-bootstrap";
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -16,7 +15,15 @@ const CategoryOptions = [
   { value: 1, label: "Challenge" },
 ];
 
-function UploadPage() {
+function UploadPage(props) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    setShow(false);
+  };
+  const handleShow = () => {
+    setShow(true);
+  };
+
   const [multipleVideosUrl, setMultipleVideosUrl] = useState("");
   const [posterUrl, setPosterUrl] = useState("");
 
@@ -35,71 +42,68 @@ function UploadPage() {
     setCategory(event.target.value);
   };
 
-  // const onDropHandler = (files) => {
-  //   let formData = new FormData();
-
-  //   const config = {
-  //     header: { "content-type": "multipart/form-data" },
-  //   };
-  //   formData.append("file", files[0]);
-  // };
-
   const onSubmitHandler = (event) => {
     event.preventDefault();
     console.log(Category);
-    const insertDate ={
-      "title":VideoTitle,
-      "description":Description,
-      "category":Category,
-      "videosUrl":multipleVideosUrl,
-      "posterUrl":posterUrl,
-      "fileName" : [],
+    const insertDate = {
+      title: VideoTitle,
+      description: Description,
+      category: Category,
+      videosUrl: multipleVideosUrl,
+      posterUrl: posterUrl,
     };
-    axios.post('http://localhost:8000/video/insert',insertDate)
-    // axios.post('http://3.39.32.4:8000/video/insert',insertDate)
-    .then((response)=>{
-       console.log(response); 
-       alert("디비 저장 ~");
-      }) 
-    .catch(error => {
-       console.log('error : ',error.response) 
+    // axios.post('http://localhost:8000/video/insert',insertDate)
+    axios
+      .post("http://3.39.32.4:8000/video/insert", insertDate)
+      .then((response) => {
+        console.log(response);
+        alert("디비 저장 ~");
       })
-    
-
+      .catch((error) => {
+        console.log("error : ", error.response);
+      });
   };
 
   return (
     <div>
-      
       <LoginNavigationBar />
       <div className={styles.root}>
         <div className={styles.title}>
           <Title level={2}>전시회 개최</Title>
         </div>
         <label>전시회 포스터</label>
-        <ImageUpload setUrl={setPosterUrl}/>
-        <br/>
+        <ImageUpload setUrl={setPosterUrl} />
+        <br />
 
         <label>작품 업로드</label>
-          <MultipleFilesUpload setUrl={setMultipleVideosUrl} />
-        <Form onSubmit={onSubmitHandler}>
-          <div className={styles.contents}>
-            {/* Drop Zone */}
+        <br />
+        <Button size="lg" type="primary" onClick={handleShow}>
+          업로드 하러가기
+        </Button>
+        {/* 업로드 모달 */}
+        <Modal
+          show={show}
+          onHide={handleClose}
+          {...props}
+          size="1g"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>비디오 추가하기</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FileUpload setUrl={setMultipleVideosUrl} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              비디오 추가하기
+            </Button>
+          </Modal.Footer>
+        </Modal>
 
-            
-            {/* <Dropzone
-              onDrop={onDropHandler}
-              multiple={false}
-              maxSize={9000000000}
-            >
-              {({ getRootProps, getInputProps }) => (
-                <div className={styles.dropZone} {...getRootProps()}>
-                  <input {...getInputProps()}  />
-                  <PlusOutlined style={{ fontSize: "3rem"}} />
-                </div>
-              )}
-            </Dropzone> */}
-          </div>
+        <Form onSubmit={onSubmitHandler}>
+          <div className={styles.contents}></div>
           <br />
 
           <label>전시회 제목</label>
@@ -126,11 +130,16 @@ function UploadPage() {
             ))}
           </select>
           <br />
-          
+
           <br />
 
           <div className={styles.submitBtn}>
-            <Button className="submitBtn" size="lg" type="primary" onClick={onSubmitHandler}>
+            <Button
+              className="submitBtn"
+              size="lg"
+              type="primary"
+              onClick={onSubmitHandler}
+            >
               전시회 개최
             </Button>
           </div>
