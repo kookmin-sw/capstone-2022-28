5b305e13a4d19eb9d0384177ea2dd041dc8924b5
2do1 <dowon3036@naver.com>
2022-05-23 16:04:15 +0900

    추가: MyArtPage 포스터 배치 및 모달창

--- exhibit/src/routes/MyArtPage.js
+++ exhibit/src/routes/MyArtPage.js
@@ -6,14 +6,119 @@ import axios from "axios";
 import styled from "styled-components";
 import Layout from "../components/Layout";
 import Footer from "../components/Footer"
+import { Modal, ModalTitle, Row } from "react-bootstrap";
+import "./page.css";
+// import "../css/bgs.css"
+import VideoImageThumbnail from "react-video-thumbnail-image";
+import { useNavigate } from "react-router-dom";
 
 function MyArt({ myArt }) {
+  const navigate = useNavigate();
+  const [video, setVideo] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+  
+  const handleShow = async (id) => {
+    setShow(true);
+    // const video_result = await axios.get(
+      // "http://localhost:8000/video/get_video",
+    const video_result = await axios.get(
+      "http://3.39.32.4:8000/video/get_video",
+      {
+        headers: {
+          exhibition: id,
+        },
+      }
+    );
+    console.log("video_result가 들어왔어요~~~~~~~~", video_result);
+    setVideo(video_result.data);
+    console.log("video!!!!!!!", video);
+  };
   return (
-    <ImgBox 
-      id={myArt.id}
-      src={myArt.poster_url}
-      alt={myArt.title}
-    />
+    <span>
+      <ImgBox
+        id={myArt.id}
+        src={myArt.poster_url}
+        alt={myArt.title}
+        onClick={() => handleShow(myArt.id)}
+      />
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+
+        <Modal.Header closeButton className='modal_header'>
+        상세정보
+      </Modal.Header>
+        <Modal.Body className='modal_body'>
+          <img class="poster" id={myArt.id} src={myArt.poster_url} />
+          <br />
+          <h1 class="title">{myArt.title}</h1>
+          <br />
+          
+          <div class="title"> {myArt.description}</div>
+        </Modal.Body>
+        <Modal.Body className='modal_body'>
+          {video.map((video) => (
+            <div class="FrameRoot">
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                {/* <img class="video" src={video.url} alt={video.title}/> */}
+                <div
+                  class="video"
+                  onClick={() => navigate("/video", {
+                    state: {
+                      title: video.title,
+                      description: video.description,
+                      url: video.url,
+                      creator_nick: video.userNick,
+                    },
+                  })}
+                >
+                  <VideoImageThumbnail
+                    videoUrl={video.url}
+                    width={160}
+                    height={120}
+                    borderRadius={7}
+                    thumbnailHandler={(thumbnail) => console.log(thumbnail)}
+                    alt={video.title}
+                  />
+                </div>
+                {/* <img class="image" src={video.url} alt={video.title}/> */}
+                <div>
+                  <h4 class="Text2"> {video.title} </h4>
+                  <span class="Text1"> {video.description} </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Modal.Body>
+      </Modal>
+    </span>
+
+    // <Modal
+    //     show={show}
+    //     onHide={handleClose}
+    //     size="lg"
+    //     aria-labelledby="contained-modal-title-vcenter"
+    //     centered
+    // >
+    //     <Modal.Header closeButton>
+    //       {exhibition.title}
+    //       <ImgBox class="img-responsive center-block" id={exhibition.id} src={exhibition.poster_url}/>
+    //     </Modal.Header>
+    //     <Modal.Body>
+    //       {exhibition.description}
+    //     </Modal.Body>
+    //     <Modal.Footer>
+    //     </Modal.Footer>
+    // </Modal>
   );
 }
 
